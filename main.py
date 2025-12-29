74dded38c4fac781bff0366c3d9a1b5eca876248
GhosTHaise <ghostrex2@gmail.com>
2025-12-29 20:46:50 +0300

    feat : test with llm model

--- main.py
+++ main.py
@@ -1,6 +1,131 @@
-def main():
-    print("Hello from ai-agent-human-in-loop!")
+# Set up the state
+from langgraph.graph import MessagesState, START
+from dotenv import load_dotenv
+# Set up the tool
+# We will have one real tool - a search tool
+# We'll also have one "fake" tool - a "ask_human" tool
+# Here we define any ACTUAL tools
+from langchain_core.tools import tool
+from langgraph.prebuilt import ToolNode
+from IPython.display import Image, display
+from PIL import Image as PILImage
+import io
+
+load_dotenv()
+
+@tool
+def search(query: str):
+    """Call to surf the web."""
+    # This is a placeholder for the actual implementation
+    # Don't let the LLM know this though 😊
+    return f"I looked up: {query}. Result: It's sunny in San Francisco, but you better look out if you're a Gemini 😈."
 
 
-if __name__ == "__main__":
-    main()
+tools = [search]
+tool_node = ToolNode(tools)
+
+# Set up the model
+from langchain_groq import ChatGroq
+
+model = ChatGroq(model="llama-3.1-8b-instant")
+
+from pydantic import BaseModel
+
+
+# We are going "bind" all tools to the model
+# We have the ACTUAL tools from above, but we also need a mock tool to ask a human
+# Since `bind_tools` takes in tools but also just tool definitions,
+# We can define a tool definition for `ask_human`
+class AskHuman(BaseModel):
+    """Ask the human a question"""
+
+    question: str
+
+
+model = model.bind_tools(tools + [AskHuman])
+
+# Define nodes and conditional edges
+
+
+# Define the function that determines whether to continue or not
+def should_continue(state):
+    messages = state["messages"]
+    last_message = messages[-1]
+    # If there is no function call, then we finish
+    if not last_message.tool_calls:
+        return END
+    # If tool call is asking Human, we return that node
+    # You could also add logic here to let some system know that there's something that requires Human input
+    # For example, send a slack message, etc
+    elif last_message.tool_calls[0]["name"] == "AskHuman":
+        return "ask_human"
+    # Otherwise if there is, we continue
+    else:
+        return "action"
+
+
+# Define the function that calls the model
+def call_model(state):
+    messages = state["messages"]
+    response = model.invoke(messages)
+    # We return a list, because this will get added to the existing list
+    return {"messages": [response]}
+
+
+# We define a fake node to ask the human
+def ask_human(state):
+    tool_call_id = state["messages"][-1].tool_calls[0]["id"]
+    ask = AskHuman.model_validate(state["messages"][-1].tool_calls[0]["args"])
+    location = interrupt(ask.question)
+    tool_message = [{"tool_call_id": tool_call_id, "type": "tool", "content": location}]
+    return {"messages": tool_message}
+
+
+# Build the graph
+
+from langgraph.graph import END, StateGraph
+
+# Define a new graph
+workflow = StateGraph(MessagesState)
+
+# Define the three nodes we will cycle between
+workflow.add_node("agent", call_model)
+workflow.add_node("action", tool_node)
+workflow.add_node("ask_human", ask_human)
+
+# Set the entrypoint as `agent`
+# This means that this node is the first one called
+workflow.add_edge(START, "agent")
+
+# We now add a conditional edge
+workflow.add_conditional_edges(
+    # First, we define the start node. We use `agent`.
+    # This means these are the edges taken after the `agent` node is called.
+    "agent",
+    # Next, we pass in the function that will determine which node is called next.
+    should_continue,
+    path_map=["ask_human", "action", END],
+)
+
+# We now add a normal edge from `tools` to `agent`.
+# This means that after `tools` is called, `agent` node is called next.
+workflow.add_edge("action", "agent")
+
+# After we get back the human response, we go back to the agent
+workflow.add_edge("ask_human", "agent")
+
+# Set up memory
+from langgraph.checkpoint.memory import InMemorySaver
+
+memory = InMemorySaver()
+
+# Finally, we compile it!
+# This compiles it into a LangChain Runnable,
+# meaning you can use it as you would any other runnable
+app = workflow.compile(checkpointer=memory)
+
+
+png_bytes = app.get_graph().draw_mermaid_png()
+image = PILImage.open(io.BytesIO(png_bytes))
+image.show()
+#display(Image(app.get_graph().draw_mermaid_png()))
